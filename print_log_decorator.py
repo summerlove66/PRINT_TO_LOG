94df3dfc28bd41dd3c1bdff3413fd19be3965174
Hui <summerlove66@users.noreply.github.com>
2017-10-19 11:09:27 +0800

    Update print_log_decorator.py

--- print_log_decorator.py
+++ print_log_decorator.py
@@ -2,23 +2,26 @@ import builtins
 from  functools import wraps, partial
 from datetime import datetime
 
-older_print = builtins.print
+raw_print = builtins.print
 
 
-def new_print(*args, **kwargs):
-    return older_print(datetime.now().strftime("%Y-%m-%d %H:%M"), *args, **kwargs)
+def new_print(*args, print_msg=True, **kwargs):
+    if print_msg:
+        raw_print(*args)
+    return raw_print(datetime.now().strftime("%Y-%m-%d %H:%M"), *args, **kwargs)
 
 
-def print_to_log(log_file):
+def print_to_log(log_file, print_msg=True):
     def decorate(func):
-        @wraps(partial)
+        @wraps(func)
         def wrapper(*args, **kwargs):
             with open(log_file, "a", encoding="utf8") as f:
-                builtins.print = partial(new_print, file=f)
+                builtins.print = partial(new_print, print_msg=print_msg, file=f)
                 res = func(*args, **kwargs)
-                builtins.print = older_print
-                return res
+                builtins.print = raw_print
+            return res
 
         return wrapper
 
     return decorate
+
